0adbdcfc34d314b682172834254d6052f9082765
Marc Nijdam <marc@helium.com>
2017-07-10 14:54:06 -0700

    Add needs_reset

--- src/Helium.cpp
+++ src/Helium.cpp
@@ -81,7 +81,6 @@ Helium::connect(struct connection * connection, uint32_t retries)
     return helium_connect(&_ctx, connection, retries);
 }
 
-
 bool
 Helium::connected()
 {
@@ -92,6 +91,12 @@ int
 Helium::sleep(struct connection * connection)
 {
     return helium_sleep(&_ctx, connection);
+}
+
+bool
+Helium::needs_reset()
+{
+    return helium_needs_reset(&_ctx);
 }
 
 int
